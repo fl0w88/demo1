8611bec78eaaaeab564838990f6da28a52d89d6a
Florin Balint <florin.balint@nagarro.com>
2021-10-03 21:44:21 +0200

    adding project demo1

---
+++ src/test/java/com/example/FirstTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package com.example;
+
+
+import org.junit.jupiter.api.*;
+
+@Tag("development")
+@Tag("stable")
+public class FirstTest {
+
+    // we define a variable ( or an object) of the type Calculator
+    private Calculator myCalculator;
+
+    // before we start, we must initialize our Calculator
+    @BeforeEach
+    public void init() {
+        // this is how we 'create' a new Calculator
+        myCalculator = new Calculator();
+    }
+
+    @Test
+    @DisplayName("My 1st JUnit 5 test! 😎 - Calculator - Addition Test")
+    void additionTest() {
+        Assertions.assertEquals(8, myCalculator.add(3, 5));
+    }
+
+}
